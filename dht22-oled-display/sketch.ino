1a114ea889d85f668ec760643bf1848e0c188489
Saumya V. <saumyavijayvargiya@gmail.com>
2026-07-11 16:07:44 +0530

    Create sketch.ino

---
+++ dht22-oled-display/sketch.ino
@@ -0,0 +1,50 @@
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include <DHT.h>
+
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+#define DHTPIN 4
+#define DHTTYPE DHT22
+
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  Serial.begin(115200);
+  dht.begin();
+
+  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
+    Serial.println("OLED not found");
+    while (true);
+  }
+  display.clearDisplay();
+}
+
+void loop() {
+  float temp = dht.readTemperature();
+  float hum = dht.readHumidity();
+
+  if (isnan(temp) || isnan(hum)) {
+    Serial.println("Failed to read from DHT sensor");
+    return;
+  }
+
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(SSD1306_WHITE);
+  display.setCursor(0, 0);
+  display.print("Temp: ");
+  display.print(temp);
+  display.println(" C");
+  display.print("Humidity: ");
+  display.print(hum);
+  display.println(" %");
+  display.display();
+
+  Serial.print("Temp: "); Serial.print(temp);
+  Serial.print(" Humidity: "); Serial.println(hum);
+
+  delay(2000);
+}
